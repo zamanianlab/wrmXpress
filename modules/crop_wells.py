1e00cbddd0da9dff04ef10778ba077a4e54d61f7
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2022-07-28 08:39:17 -0500

    automate well naming

--- modules/crop_wells.py
+++ modules/crop_wells.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from string import ascii_uppercase
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,8 +12,10 @@ from skimage.transform import hough_circle, hough_circle_peaks
 from skimage.morphology import dilation, reconstruction
 
 
-def crop_wells(g):
-
+def auto_crop(g):
+    ''' 
+    Use Hough transform to find and crop circular wells
+    '''
     wells_per_image = g.image_n_row * g.image_n_col
 
     vid_path = Path.home().joinpath(g.plate_dir, g.plate + '.avi')
@@ -109,16 +112,27 @@ def crop_wells(g):
 
     lbl, objects = ndimage.label(filled)
     centers = ndimage.center_of_mass(filled, lbl, range(
-        1, 1 + wells_per_image, 1)) 
+        1, 1 + wells_per_image, 1))
 
     # make a data frame with well names linked to coordinates of centers
-    # this needs to be able handle differnet plate formats
     well_names = pd.DataFrame(centers, columns=['y', 'x'])
     well_names = well_names.sort_values(by=['y'])
-    well_names['row'] = ['A'] * 6 + ['B'] * 6 + ['C'] * 6 + ['D'] * 6
+    row_names = list(ascii_uppercase[0:g.image_n_row])
+    col_names = list(range(1, g.image_n_col + 1, 1))
+
+    row_names_df = []
+    for name in row_names:
+        for col in range(g.image_n_col):
+            row_names_df.append(name)
+
+    col_names_df = []
+    for name in col_names:
+        for row in range(g.image_n_row):
+            col_names_df.append(str(name).zfill(2))
+
+    well_names['row'] = row_names_df
     well_names = well_names.sort_values(by=['x'])
-    well_names['col'] = ['01'] * 4 + ['02'] * 4 + \
-        ['03'] * 4 + ['04'] * 4 + ['05'] * 4 + ['06'] * 4
+    well_names['col'] = col_names_df
     well_names['well'] = well_names['row'] + well_names['col']
 
     well_arrays = {}
@@ -137,11 +151,44 @@ def crop_wells(g):
 
     g = g._replace(time_points=vid_array.shape[0])
 
+    create_htd(vid_array, well_names)
+
+    return g
+
+
+def grid_crop(g):
+    ''' 
+    Crop wells based on the image size and number of wells
+    '''
+    wells_per_image = g.image_n_row * g.image_n_col
+
+    vid_path = Path.home().joinpath(g.plate_dir, g.plate + '.avi')
+
+    vid = cv2.VideoCapture(str(vid_path))
+    ret = True
+    frames = []
+    while ret:
+        ret, img = vid.read()
+        if ret:
+            frames.append(img)
+
+    vid_array = np.stack(frames, axis=0)
+    timepoint, height, width = vid_array.shape
+
+    x_interval = width / g.image_n_row
+    y_interval = height / g.image_n_col
+
+
+    create_htd(vid_array, well_names)
+
+
+def create_htd(array, df):
+
     # make HTD for non-IX data
     lines = []
-    lines.append('"TimePoints", ' + str(vid_array.shape[0]) + "\n")
-    lines.append('"XWells", ' + str(len(pd.unique(well_names['col']))) + "\n")
-    lines.append('"YWells", ' + str(len(pd.unique(well_names['row']))) + "\n")
+    lines.append('"TimePoints", ' + str(array.shape[0]) + "\n")
+    lines.append('"XWells", ' + str(len(pd.unique(df['col']))) + "\n")
+    lines.append('"YWells", ' + str(len(pd.unique(df['row']))) + "\n")
     lines.append('"XSites", ' + "1" + "\n")
     lines.append('"YSites", ' + "1" + "\n")
     lines.append('"NWavelengths", ' + "1" + "\n")
@@ -150,5 +197,3 @@ def crop_wells(g):
     htd_path = g.plate_dir.joinpath(g.plate_short + '.HTD')
     with open(htd_path, mode='w') as htd_file:
         htd_file.writelines(lines)
-
-    return g
