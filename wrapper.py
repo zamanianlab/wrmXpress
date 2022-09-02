47bab45aefd7ed1e37ffb643f8fc0f593e364bd9
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2022-09-02 13:22:43 -0500

    cellpose happens before filelist generation

--- wrapper.py
+++ wrapper.py
@@ -73,22 +73,25 @@ if __name__ == "__main__":
     ########################################
 
     if 'cellprofiler' in modules.keys():
+        pipeline = modules['cellprofiler']['pipeline'][0]
+        
         # rename TIF to tif to work with cellpose
         for filepath in Path('input/20220804-p04-KTR_1632/TimePoint_1').glob('**/*'):
             os.rename(filepath, str(filepath).replace('TIF', 'tif'))
         wells = [well.replace('TIF', 'tif') for well in wells]
         g = g._replace(wells=wells)
-        pipeline = modules['cellprofiler']['pipeline'][0]
+
+        if 'cellpose' in pipeline:
+            cellpose_command = 'python -m cellpose --dir {}/{}/TimePoint_1 --pretrained_model wrmXpress/cp_pipelines/cellpose_models/20220830_all --diameter 0 --save_png --no_npy --verbose'.format(g.input, g.plate)
+            cellpose_command_split = shlex.split(cellpose_command)
+            subprocess.run(cellpose_command_split)
+
         fl_command = 'Rscript wrmXpress/scripts/cp/generate_filelist_{}.R {} {}'.format(
             pipeline, g.plate, g.wells)
         fl_command_split = shlex.split(fl_command)
         print('Generating file list for CellProfiler.')
         subprocess.run(fl_command_split)
 
-        if 'cellpose' in pipeline:
-            cellpose_command = 'python -m cellpose --dir {}/{}/TimePoint_1 --pretrained_model wrmXpress/cp_pipelines/cellpose_models/20220830_all --diameter 0 --save_png --no_npy --verbose'.format(g.input, g.plate)
-            cellpose_command_split = shlex.split(cellpose_command)
-            subprocess.run(cellpose_command_split)
         cellprofiler_command = 'cellprofiler -c -r -p wrmXpress/cp_pipelines/pipelines/{}.cppipe --data-file=input/image_paths_{}.csv'.format(
             pipeline, pipeline)
         cellprofiler_command_split = shlex.split(cellprofiler_command)
