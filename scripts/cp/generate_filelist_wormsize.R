d5eb7abcbaf3ca5d6750db297b9eaf9399d70a69
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-02-13 11:29:21 -0600

    few small fixes to file generator

--- scripts/cp/generate_filelist_wormsize.R
+++ scripts/cp/generate_filelist_wormsize.R
@@ -10,12 +10,9 @@ input <- args[1]
 plate <- args[2]
 wells <- args[3:length(args) - 1] %>% stringr::str_remove_all(., "[,|\\[|\\]]")
 
-
 image_dir <- stringr::str_c(input, plate, sep = "/")
-print(image_dir)
 
 input_files <- list.files(path = image_dir, pattern = ".*TIF$", recursive = TRUE) %>% magrittr::extract(dplyr::matches(wells, vars = .))
-print(input_files)
 mask <- "well_mask.png"
 
 wd <- getwd() %>% str_remove(., "^/")
@@ -23,11 +20,11 @@ wd <- getwd() %>% str_remove(., "^/")
 load_csv <- dplyr::tibble(
   Group_Number = 1,
   Group_Index = seq(1, length(input_files)),
-  URL_RawImage = stringr::str_c("file:", input, plate, input_files, sep = "/"),
-  URL_WellMask = stringr::str_c("file:", "/wrmXpress", "cp_pipelines", "masks", mask, sep = "/"),
+  URL_RawImage = stringr::str_c("file:", substring(input, 2), plate, input_files, sep = "/"),
+  URL_WellMask = stringr::str_c("file:", "wrmXpress", "cp_pipelines", "masks", mask, sep = "/"),
   PathName_RawImage = stringr::str_remove(URL_RawImage, pattern = "/[^/]*$") %>% str_remove(., "file:"),
   PathName_WellMask = stringr::str_remove(URL_WellMask, mask) %>% str_remove(., "file:") %>% str_remove(., "/$"),
-  FileName_RawImage = input_files,
+  FileName_RawImage = input_files %>% str_remove(., "TimePoint_[0-9]*//"),
   FileName_WellMask = mask,
   Series_RawImage = 0,
   Series_WellMask = 0,
