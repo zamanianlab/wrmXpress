f8f861e8f79f2da2511e657004c3308a63d37d0c
ankitjk <ankitjkul@gmail.com>
2024-04-06 17:54:08 -0500

    fixed bugs in optical flow

--- pipelines/optical_flow.py
+++ pipelines/optical_flow.py
@@ -3,32 +3,34 @@ import numpy as np
 import os
 
 def optical_flow(g, wells, options):
-    length = len(wells)
-    first_image = cv2.imread(os.path.join(g.plate_dir, 'Timepoint_1', g.plate_short + f'_{wells[0]}_w1.TIF'))
-    height, width = first_image.shape[:2]
-
-    # create empty np array to store all magnitudes
-    all_mag = np.zeros((length - 1, height, width))
-
     total_mag = 0
 
-    # loop through all (well, wavelength pairings)
+    # loop through all (well, wavelength) pairings
     for well in wells:
         for wavelength in range(g.n_waves):
+            # create empty list to store magnitude arrays
+            all_mag = []
+
+            # loop through all timepoints
             for timepoint in range(g.time_points - 1):
                 # get path of frame 1 and frame 2
-                frame1 = cv2.imread(os.path.join(g.plate_dir, f'TimePoint_{timepoint + 1}', g.plate_short + f'_{well}_w{wavelength + 1}.TIF'))
-                frame2 = cv2.imread(os.path.join(g.plate_dir, f'TimePoint_{timepoint + 2}', g.plate_short + f'_{well}_w{wavelength + 1}.TIF'))
+                frame1 = cv2.imread(os.path.join(g.plate_dir, f'TimePoint_{timepoint + 1}', f'{g.plate_short}_{well}_w{wavelength + 1}.TIF'))
+                frame2 = cv2.imread(os.path.join(g.plate_dir, f'TimePoint_{timepoint + 2}', f'{g.plate_short}_{well}_w{wavelength + 1}.TIF'))
+
+                # convert frames to grayscale
+                frame1_gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+                frame2_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
                 # calculate optical flow
-                # flow = cv2.calcOpticalFlowFarneback(frame1, frame2, options['pyr_scale'], options['levels'], options['winsize'], options['iterations'], options['poly_n'], options['poly_sigma'], options['flow'], options['flags'])
-                flow = cv2.calcOpticalFlowFarneback(frame1, frame2, None, 0.9, 10, 2, 7, 1, 0.7, 0)
+                flow = cv2.calcOpticalFlowFarneback(frame1_gray, frame2_gray, None, options['levels'], options['winsize'], options['iterations'], options['poly_n'], options['poly_sigma'], options['flow'], options['flags'])
 
                 # calculate magnitude of optical flow vectors
                 magnitude = np.sqrt(flow[..., 0]**2 + flow[..., 1]**2)
-                # add sum of magnitude values in array to total_mag
+
+                # add sum of magnitude values to total_mag
                 total_mag += np.sum(magnitude)
-                # store magnitude in all_mag
+
+                # store magnitude array in all_mag
                 all_mag.append(magnitude)
         
             # calculate total flow across the entire array
@@ -40,7 +42,7 @@ def optical_flow(g, wells, options):
             # save flow image in 'work/optical_flow'
             out_dir = os.path.join(g.work, 'optical_flow')
             os.makedirs(out_dir, exist_ok=True)
-            outpath = os.join(out_dir, g.plate_short + f'_{well}_w{wavelength}.png')
-            cv2.imwrite(str(outpath), sum_blur)
+            outpath = os.path.join(out_dir, f'{g.plate_short}_{well}_w{wavelength + 1}.png')
+            cv2.imwrite(outpath, sum_blur)
 
-            return total_mag
+    return total_mag
